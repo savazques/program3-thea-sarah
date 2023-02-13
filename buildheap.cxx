0d5d82d142bce2ad6d5da876f2805dd7348dc355
savaques <savasquez@ucdavis.edu>
2023-02-12 20:57:41 -0800

    yee yee
    Co-authored-by: thea-mccallie <thea-mccallie@users.noreply.github.com>

--- buildheap.cxx
+++ buildheap.cxx
@@ -1,2 +1,32 @@
+#include "priorityqueue.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+
 int main(int argc, char** argv) {
+
+    PriorityQueue myQueue(30); 
+    std::string jsonfile = argv[1]; 
+    std::ifstream file (jsonfile); 
+    nlohmann::json Key_Instruction = nlohmann::json::parse(file); 
+
+    for(nlohmann::json::iterator it = Key_Instruction.begin(); it != Key_Instruction.end(); it++)
+    {
+        if(it.key() != "metadata")
+        {
+            //nlohmann::json d = Key_Instruction[it.key()];
+            //if the
+            if (!Key_Instruction[it.key()]["key"].is_null()){
+                
+                Key tempKey = Key_Instruction[it.key()]["key"];  
+                myQueue.insert(tempKey); 
+            }
+            else {
+                myQueue.removeMin();
+            };
+        };
+    };
+    nlohmann::json OutputJSON; 
+    OutputJSON = myQueue.JSON(); 
+    std::cout << OutputJSON.dump(2) << std::endl; 
 }
--- priorityqueue.cpp
+++ priorityqueue.cpp
@@ -14,24 +14,48 @@ void PriorityQueue::insert(Key k) {
 }
 
 void PriorityQueue::insert(KeyValuePair kv) {
-	// TODO: complete this function
+    //insert the keyvalue pair into vector
+    nodes_.push_back(kv); 
+    //get the size of the vector
+    size_t s = size(); 
+    //heapify the last element to its correct place in heap
+    heapifyUp(s);
 }
 
 KeyValuePair PriorityQueue::min() {
-	// TODO: complete this function
+    // returning the top node, which is the min as the heap is a min heap
+	return nodes_[1]; 
 }
 
 KeyValuePair PriorityQueue::removeMin() {
-	// TODO: complete this function
+    // storing the min 
+    KeyValuePair tempMin = nodes_[1]; 
+    //get the size of the vector
+    size_t s = size(); 
+    //swap the top of the heap with the last vector
+	nodes_[1] = nodes_[s]; 
+    //remove the last node
+    nodes_.pop_back(); 
+    //heapify the new number to its correct position
+    heapifyDown(1); 
+
+    return tempMin; 
 }
 
 bool PriorityQueue::isEmpty() const {
-	// TODO: complete this function
-    
+    //if size of the vector is 0 that means it is empty and return true
+	if (nodes_.size() == 0)  { 
+        return true; 
+    }
+    //else we return false
+    else {
+        return false; 
+    };
 }
 
 size_t PriorityQueue::size() const {
-	// TODO: complete this function
+    //return the size of the vector 
+    return nodes_.size();
 }
 
 nlohmann::json PriorityQueue::JSON() const {
@@ -58,17 +82,118 @@ nlohmann::json PriorityQueue::JSON() const {
 }
 
 void PriorityQueue::heapifyUp(size_t i) {
-	// TODO: complete this function
+    //dont heapify if you are at the root
+    if (i != 1){ 
+        //variable that holds child info
+        KeyValuePair child = nodes_[i];
+        //varibale that hoild parent info
+        KeyValuePair parentVector = nodes_[i/2]; 
+        //check if the child is smaller than the parent 
+        if (child.first < parentVector.first){
+            //the child becomes the parent
+            nodes_[i] = parentVector; 
+            //the parent becomes the child
+            nodes_[i/2] = child; 
+            
+        };
+    };
+    // using recursion to continue to check the parent if such a par
+    if (i != 1){
+        heapifyUp(i/2); 
+    }; 
 }
 
 void PriorityQueue::heapifyDown(size_t i) {
-	// TODO: complete this function
-}
+    //check if the size of nodes is equal to left child index that case that the node only has one child
+    if (nodes_.size() == (i * 2)){
+        //create variable that holdes the KVP of left child
+        KeyValuePair leftchild = nodes_[(i*2)]; 
+        //create variable that holds KVP of parent 
+        KeyValuePair Parent = nodes_[i];
+        //if parent is bigger than left child we switch 
+        if(leftchild.first < Parent.first)
+        {
+        //the parent becomes the left child 
+            nodes_[i] = leftchild; 
+        //the left child becomes the parent
+            nodes_[i*2] = Parent;
+        }
+        
+            
+     
+    }
+    // in the case that the ith node has two children 
+	if(nodes_.size() > ((i * 2)+1)){
+        // assigning a child1, a KeyValuePair variable to the value of the ith's node first child
+        KeyValuePair child1 = nodes_[i * 2]; 
+        // assigning a child1, a KeyValuePair variable to the value of the ith's node second child
+        KeyValuePair child2 = nodes_[(i * 2) + 1];
+        // assigning a child1, a KeyValuePair variable to the value of the ith node 
+        KeyValuePair parent = nodes_[i]; 
+        // in the case that both children are smaller than the parent 
+        if ((child1.first < parent.first) && (child2.first < parent.first)){
+            // in the case that the first child is smaller than the second
+            if (child1.first < child2.first){
+                // child node becomes parent node 
+                nodes_[i *2] = parent; 
+                // parent node becomes child node 
+                nodes_[i] = child1; 
+            };
+            // in the case that the second child is smaller than the first
+            if (child2.first < child1.first){
+                // child node becomes parent mode 
+                nodes_[(i * 2) +1] = parent; 
+                // parent node becomes the child node 
+                nodes_[i] = child2; 
+            };
+            // exit if it was both children that were smaller than the parent 
+            exit(0); 
+        };
+        // in the case that only one child is smaller than the parent (ensured by the exit statment earlier)
+        if ((child1.first < parent.first) || (child2.first < parent.first)){
+            // in the case that it is the first child that is smaller 
+            if (child1.first < parent.first){
+                // child node becomes parent node 
+                nodes_[i *2] = parent; 
+                // parent node becomes child node 
+                nodes_[i] = child1; 
+            };
+            // in the case that it is the second child that is smaller than the parent 
+            if (child2.first < parent.first){
+                // child node becomes parent mode 
+                nodes_[(i * 2) +1] = parent; 
+                // parent node becomes the child node 
+                nodes_[i] = child2; 
+            };
+        };
+    };
+    // using recursion to continue to check the children of node i if such chiden exist
+    if (nodes_.size() > ((i * 2) + 1)){ 
+        // specifically child one (left)
+        heapifyDown( i * 2); 
+        // specifically child two (right) 
+        heapifyDown((i * 2) + 1); 
+    }; 
+};
 
 void PriorityQueue::removeNode(size_t i) {
-	// TODO: complete this function
+   // variable that holds the size of the node
+    size_t s = size();
+   //swapping the last node with the node we want to remove
+    nodes_[i] = nodes_[s]; 
+    //pop the last node
+    nodes_.pop_back();
+    //heapify down if needed
+    heapifyDown(i);  
+    //heapify up if needed 
+    heapifyUp(i); 
+} 
+Key PriorityQueue::getKey(size_t i) {
+    // making a KeyValuePair variable to hold the node as to access it's key later
+    KeyValuePair tempVector = nodes_[i]; 
+    // accessing the key and returning it 
+    return tempVector.first; 
 }
 
-Key PriorityQueue::getKey(size_t i) {
-	// TODO: complete this function
-}
+
+// del vector[index]
