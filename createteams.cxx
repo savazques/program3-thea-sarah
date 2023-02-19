b9a9c86b25aa7d3ad2084a0587118fb7cb4ddb40
savaques <savasquez@ucdavis.edu>
2023-02-18 23:21:30 -0800

    better understanding i think

--- createteams.cxx
+++ createteams.cxx
@@ -13,27 +13,45 @@ int main(int argc, char** argv) {
     std::map<int,int> TEAMS; 
     double currentWinPercentage; 
     double difference;
-    int restcheck = 0; 
+    PriorityQueue teamPlayerQueue(20);
+  
     double lowest = 51.0;
     
-
-    for(nlohmann::json::iterator it = teamData.begin(); it != teamData.end(); it++)
+    //while the sizeoftheque is still less than the teams
+    while(teamPlayerQueue.size() < 20)
     {
-        if(it.key() != "metadata")
+        for(nlohmann::json::iterator it = teamData.begin(); it != teamData.end(); it++)
         {
-            for (int i  = 0; i < 40; i ++)
+            if(it.key() != "metadata")
             {
-                nlohmann::json o = teamData[it.key()][i] ;
-                std::cout << o["playerTwo"]; 
+                for (int i  = 0; i < 40; i ++)
+                {
+                    nlohmann::json round = teamData[it.key()][i] ;
+                    int player1 = round["playerOne"]; 
+                    int player2 = round["playerTwo"]; 
+                    double difference = abs(round["winPercentage"] - 50); 
 
+                    if(lowest > difference)
+                    {
+                        lowest = difference;
+                        Value players(player1,player2);
+                        Key heapValue = lowest; 
+                        KeyValuePair kvp (heapValue,players);
+                        teamPlayerQueue.insert(kvp);
+                        
+                    }
+
+                    
+                }
                 
             }
             
+                
         }
         
-
-          
     }
+
+
 
 
 }
